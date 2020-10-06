e79b30e4fa85013f57868e16ff96b178c22aacab
Alex <nyt604@gmail.com>
2020-10-06 19:12:56 +0200

    Fix population script creating duplicate entries
    added IGNORE so that if the id exists it "fails" but will keep on running, if the id does not exists is runs as expected and populates

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,7 +1,7 @@
-INSERT INTO activities (name, description, price, group_size, duration, start_time, age_restriction, height_restriction,
+INSERT IGNORE INTO activities (id, name, description, price, group_size, duration, start_time, age_restriction, height_restriction,
                         full_description)
-VALUES ('GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
-       ('Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
-       ('Sumo Wrestling', 'its like wrestling but you\'re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
-       ('Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
-       ('Mini-Golf2', 'Explore our indoor2 a22222dventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150, 'blalbla');
+VALUES (1, 'GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
+       (2, 'Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
+       (3, 'Sumo Wrestling', 'its like wrestling but you\'re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
+       (4, 'Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
+       (5, 'Mini-Golf2', 'Explore our indoor2 a22222dventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150, 'blalbla');
