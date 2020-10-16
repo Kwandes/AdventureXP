b7d94223a0e275d5269fbbf4b62b2c0e3e5940ee
geor1812 <george.ursachi7@gmail.com>
2020-10-16 14:42:15 +0200

    changed to live DB

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,13 +1,8 @@
--- THIS IS THE POPULATION SCRIP FOR THE TESTING H2 DATABASE, DO NOT CHANGE VALUES --
-INSERT INTO activities (name, description, price, group_size, duration, start_time, age_restriction, height_restriction, full_description)
-VALUES ('GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
-       ('Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
-       ('Sumo Wrestling', 'its like wrestling but you''re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
-       ('Mini-Golf', 'Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
-       ('nameTest1', ' descriptionTest1', 0, 'groupSizetest1', 30, 'startTimeTest1', 10, 150, 'fullDescriptionTest1');
+-- THIS IS THE POPULATION SCRIP FOR LIVE DATABASE CHANGE ONLY WITH TEAM APPROVAL --
+INSERT IGNORE INTO activities (id, name, description, price, group_size, duration, start_time, age_restriction, height_restriction, full_description)
 
-INSERT INTO activity_schedule (id, time)
-VALUES (1, '09:00:00'),
-       (2, '12:00:00'),
-       (3, '18:00:00'),
-       (4, '21:00:00');
+VALUES (1, 'GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
+       (2, 'Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
+       (3, 'Sumo Wrestling', 'its like wrestling but you''re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
+       (4, 'Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
+       (5, 'nameTest1', 'descriptionTest1', 0, 'groupSizetest1', 30, 'startTimeTest1', 10, 150, 'fullDescriptionTest1');
