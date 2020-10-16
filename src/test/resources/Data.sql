244a33fb58b3ca47487ae6a04fc241b727e0313b
AlexM <nyt604@gmail.com>
2020-10-16 13:43:14 +0200

    Tweak Data.sql pop scripts to be a little shorter
    too long for mycli ;-; *sad* :(

--- src/test/resources/Data.sql
+++ src/test/resources/Data.sql
@@ -3,7 +3,7 @@ INSERT INTO activities (name, description, price, group_size, duration, start_ti
 VALUES ('GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
  ('Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
  ('Sumo Wrestling', 'its like wrestling but you''re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
- ('Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
+ ('Mini-Golf', 'Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
  ('nameTest1', ' descriptionTest1', 0, 'groupSizetest1', 30, 'startTimeTest1', 10, 150, 'fullDescriptionTest1');
  
  INSERT INTO activity_schedule (id, time) 
