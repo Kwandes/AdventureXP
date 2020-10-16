e077ad12d5261e95f7e8924cc62a25150058c5c9
Barbara Rekowska <basnow28@gmail.com>
2020-10-16 15:33:09 +0200

    Updating Data.sql, aligning pictures on the front page

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -4,5 +4,4 @@ INSERT IGNORE INTO activities (id, name, description, price, group_size, duratio
 VALUES (1, 'GoKart', 'Accelerate your dreams!', 0, '1-100', 30, 'Mondays from 0-0', 10, 150, 'blabla'),
        (2, 'Paintball', 'its like COD', 400, '10-40', 30, 'Mondays from 0-0', 15, 150, 'blabla'),
        (3, 'Sumo Wrestling', 'its like wrestling but you''re fat', 400, '2-4', 10, 'Wednesday from 0-0', 15, 150, 'blabla'),
-       (4, 'Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla'),
-       (5, 'nameTest1', 'descriptionTest1', 0, 'groupSizetest1', 30, 'startTimeTest1', 10, 150, 'fullDescriptionTest1');
+       (4, 'Mini-Golf', 'Explore our indoor adventure mini-golf course. Cheapest 18-hole putting in town!', 0, '1-100', 30, 'Tuesdays from 0-0', 10, 150,'blabla');
